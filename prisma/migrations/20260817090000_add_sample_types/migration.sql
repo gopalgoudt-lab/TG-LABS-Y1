363995098ff768c325a31521ccc5552b8087bb44
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-17 17:37:51 +0530

    Fix Prisma migration order for sample type fields

--- prisma/migrations/20260817090000_add_sample_types/migration.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "DiagnosticTest"
-ADD COLUMN "sampleTypes" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN "sampleTypeOther" TEXT;
-
-ALTER TABLE "DiagnosticPackage"
-ADD COLUMN "sampleTypes" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN "sampleTypeOther" TEXT;
