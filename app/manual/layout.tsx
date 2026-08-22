b65aab6027c325d9e2e406f10fae70b1e53a5049
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-22 13:00:08 +0530

    Show Report Delivered Yes No beside reports

--- app/manual/layout.tsx
+++ app/manual/layout.tsx
@@ -1,9 +1,11 @@
 import './manual.css';
 import ManualAccessBar from './ManualAccessBar';
+import ReportDeliveryControl from './ReportDeliveryControl';
 
 export default function ManualLayout({children}:{children:React.ReactNode}){
   return <div className="thyrocare-manual">
     <ManualAccessBar/>
+    <ReportDeliveryControl/>
     {children}
   </div>;
 }
