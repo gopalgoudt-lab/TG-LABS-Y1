fac9f9d1f05ba8290fa2887e766fc00dcf16f02f
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-22 16:12:21 +0530

    Enable percentage discount dropdown across Thyrocare manual screens

--- app/manual/layout.tsx
+++ app/manual/layout.tsx
@@ -1,11 +1,13 @@
 import './manual.css';
 import ManualAccessBar from './ManualAccessBar';
 import ReportDeliveryControl from './ReportDeliveryControl';
+import DiscountPercentEnhancer from './DiscountPercentEnhancer';
 
 export default function ManualLayout({children}:{children:React.ReactNode}){
   return <div className="thyrocare-manual">
     <ManualAccessBar/>
     <ReportDeliveryControl/>
+    <DiscountPercentEnhancer/>
     {children}
   </div>;
 }
