0ad322d5d6f40e0b41979e5fe3a2ebcd1f9fa16c
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-22 16:19:12 +0530

    Enable bill and report WhatsApp sharing controls

--- app/manual/layout.tsx
+++ app/manual/layout.tsx
@@ -2,12 +2,14 @@ import './manual.css';
 import ManualAccessBar from './ManualAccessBar';
 import ReportDeliveryControl from './ReportDeliveryControl';
 import DiscountPercentEnhancer from './DiscountPercentEnhancer';
+import BillReportShareEnhancer from './BillReportShareEnhancer';
 
 export default function ManualLayout({children}:{children:React.ReactNode}){
   return <div className="thyrocare-manual">
     <ManualAccessBar/>
     <ReportDeliveryControl/>
     <DiscountPercentEnhancer/>
+    <BillReportShareEnhancer/>
     {children}
   </div>;
 }
