cad3545d7dbc81bee2a84b2994d098e1559a092c
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-23 09:24:19 +0530

    Enable delete controls for uploaded files

--- app/manual/layout.tsx
+++ app/manual/layout.tsx
@@ -3,6 +3,7 @@ import ManualAccessBar from './ManualAccessBar';
 import ReportDeliveryControl from './ReportDeliveryControl';
 import DiscountPercentEnhancer from './DiscountPercentEnhancer';
 import BillReportShareEnhancer from './BillReportShareEnhancer';
+import DeleteUploadedFilesEnhancer from './DeleteUploadedFilesEnhancer';
 
 export default function ManualLayout({children}:{children:React.ReactNode}){
   return <div className="thyrocare-manual">
@@ -10,6 +11,7 @@ export default function ManualLayout({children}:{children:React.ReactNode}){
     <ReportDeliveryControl/>
     <DiscountPercentEnhancer/>
     <BillReportShareEnhancer/>
+    <DeleteUploadedFilesEnhancer/>
     {children}
   </div>;
 }
