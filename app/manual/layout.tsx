cc70b8bf7b603b9de6ad7a03a3b7947af6e06831
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-23 10:15:25 +0530

    Enable Manual Dashboard order workflow tracker

--- app/manual/layout.tsx
+++ app/manual/layout.tsx
@@ -4,6 +4,7 @@ import ReportDeliveryControl from './ReportDeliveryControl';
 import DiscountPercentEnhancer from './DiscountPercentEnhancer';
 import BillReportShareEnhancer from './BillReportShareEnhancer';
 import DeleteUploadedFilesEnhancer from './DeleteUploadedFilesEnhancer';
+import OrderWorkflowEnhancer from './OrderWorkflowEnhancer';
 
 export default function ManualLayout({children}:{children:React.ReactNode}){
   return <div className="thyrocare-manual">
@@ -12,6 +13,7 @@ export default function ManualLayout({children}:{children:React.ReactNode}){
     <DiscountPercentEnhancer/>
     <BillReportShareEnhancer/>
     <DeleteUploadedFilesEnhancer/>
+    <OrderWorkflowEnhancer/>
     {children}
   </div>;
 }
