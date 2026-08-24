374bd705ee8b8d5d867447aa9d0a14a99ab0ca74
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-08-24 07:36:00 +0530

    Keep private TG Labs routes out of search engines

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,20 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://www.tglabs.in/sitemap.xml',
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/admin',
+        '/auth',
+        '/checkout',
+        '/manual',
+        '/patient',
+        '/technician',
+        '/api',
+      ],
+    },
+    sitemap: 'https://tglabs.in/sitemap.xml',
+    host: 'https://tglabs.in',
   };
 }
