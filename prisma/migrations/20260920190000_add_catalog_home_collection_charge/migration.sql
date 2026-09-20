1d1758808938587867c444a553521796e1eea7ca
gopalgoudt-lab <gopalgoud.t@gmail.com>
2026-09-20 19:11:16 +0530

    db: add catalog home collection charge fields

---
+++ prisma/migrations/20260920190000_add_catalog_home_collection_charge/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "DiagnosticTest" ADD COLUMN "homeCollectionCharge" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "DiagnosticPackage" ADD COLUMN "homeCollectionCharge" INTEGER NOT NULL DEFAULT 0;
